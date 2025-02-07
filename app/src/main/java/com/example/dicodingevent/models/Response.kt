8f2a12c049a9578847e73747fa8f2aaa4ea8001c
Rendy Pratama <rendypratama752@gmail.com>
2025-02-07 23:09:55 +0700

    add: models

---
+++ app/src/main/java/com/example/dicodingevent/models/Response.kt
@@ -0,0 +1,17 @@
+package com.example.dicodingevent.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EventListResponse (
+    val error: Boolean,
+    val message: String,
+    val listEvents: List<Event>
+)
+
+@Serializable
+data class EventDetailResponse (
+    val error: Boolean,
+    val message: String,
+    val event: Event
+)
