972ea4661b03382b17899b8fb8b57eb34304e2e1
Rendy Pratama <rendypratama752@gmail.com>
2025-02-19 11:30:55 +0700

    refactor: set BASE_URL as BuildConfig field

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        buildConfigField("String", "BASE_URL", "\"https://event-api.dicoding.dev\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -29,14 +30,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
--- app/src/main/java/com/example/dicodingevent/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/dicodingevent/data/remote/retrofit/ApiConfig.kt
@@ -3,17 +3,16 @@ package com.example.dicodingevent.data.remote.retrofit
 import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.dicodingevent.BuildConfig
 
 object ApiConfig {
-    private const val BASE_URL = "https://event-api.dicoding.dev"
-
     private val gson = GsonBuilder()
         .setDateFormat("yyyy-MM-dd HH:mm:ss")
         .create()
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
